180a5e84456ac2ca119f5d5ede9bf0a55045170b
Artem Silka <silkartem@gmail.com>
2020-07-27 17:44:05 -0400

    Heroku stuff

--- frontend/puntiresse.jsx
+++ frontend/puntiresse.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
+    const root = document.getElementById("roottt");
     ReactDOM.render(<h1>Test</h1>, root);
 });
