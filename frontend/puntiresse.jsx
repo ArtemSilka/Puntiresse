55a9cf9fb69db9a74617c54d4c6c111541c82232
Artem Silka <silkartem@gmail.com>
2020-07-28 10:51:34 -0400

    added reducers and store

--- frontend/puntiresse.jsx
+++ frontend/puntiresse.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+
+    // TEST
+    window.store = store;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    // TEST
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Test</h1>, root);
 });
